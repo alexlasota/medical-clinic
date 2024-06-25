dae306520cf4d4d3a0cfecf2aa0c12cecc3c4236
alexlasota <alexylasota@gmail.com>
2024-06-25 13:18:43 +0200

    liquibase yaml files

--- src/main/java/com/alexlasota/medicalclinic/mapper/PatientMapper.java
+++ src/main/java/com/alexlasota/medicalclinic/mapper/PatientMapper.java
@@ -1,14 +1,21 @@
 package com.alexlasota.medicalclinic.mapper;
 
+import com.alexlasota.medicalclinic.model.MedicalUser;
 import com.alexlasota.medicalclinic.model.Patient;
 import com.alexlasota.medicalclinic.model.PatientDto;
+import com.alexlasota.medicalclinic.model.UserDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
+
+    @Mapping(source = "medicalUser", target = "userDto")
     PatientDto patientToPatientDto(Patient patient);
+
+    UserDto userToUserDto(MedicalUser medicalUser);
 
     List<PatientDto> mapListToDto(List<Patient> patientList);
 }
